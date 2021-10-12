dc4ddc7de00cedebc7f998736abf4318ea266ebd
Pedro Gabriel <gaboleal123@gmail.com>
2021-10-12 19:14:58 -0400

    repoert

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -114,15 +114,15 @@ class ReportController extends Controller
     {
 
         $grupos = DB::table('groups_users')->where('id_user', Auth::user()->id)->pluck('id_group');
-        //d($grupos);
+
         $asistencia = Asistencia::leftjoin('users as us','asistencias.id_user','us.id')
                     ->leftjoin('users_groups as ug','us.id','ug.id_user')
                     ->leftjoin('groups as gr','ug.id_group','gr.id')
                     ->select('asistencias.*')
-                    ->whereIn('id_group',$grupos)
+                    ->whereIn('gr.id',$grupos)
                     ->orderBy('asistencias.fecha','DESC')
                     ->get();
-        //dd($asistencia);
+
         return view('pages.report.index',compact('asistencia'));
     }
 }
