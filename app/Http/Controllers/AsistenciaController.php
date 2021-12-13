7d62245ae61d4427eefe354973d1ddd81db1cfec
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-12-13 12:28:45 -0400

    validacion true

--- app/Http/Controllers/AsistenciaController.php
+++ app/Http/Controllers/AsistenciaController.php
@@ -41,21 +41,24 @@ class AsistenciaController extends Controller
      */
     public function store(Request $request)
     {
-        //try{
+        try{
             $img = $request->image;
-            $path = public_path().'/img/avatar/';
+            $fileName = '';
+            if($img != null || !empty($img)){
+                $path = public_path().'/img/avatar/';
 
-            $image_parts = explode(";base64,", $img);
-            $image_type_aux = explode("image/", $image_parts[0]);
-            $image_parts[0] = isset($image_type_aux[1]) ? $image_type_aux[1] : null;
-            //$image_type = $image_type_aux[1];
+                $image_parts = explode(";base64,", $img);
+                $image_type_aux = explode("image/", $image_parts[0]);
+                $image_parts[0] = isset($image_type_aux[1]) ? $image_type_aux[1] : null;
 
-            $image_base64 = base64_decode($image_parts[1]);
-            $fileName = uniqid() . '.png';
+                $image_base64 = base64_decode($image_parts[1]);
+                $fileName = uniqid() . '.png';
 
-            $file = $path . $fileName;
-            file_put_contents($file, $image_base64);
-            //dd($request->tipo);
+                $file = $path . $fileName;
+                file_put_contents($file, $image_base64);
+            }
+        
+
             if($request->tipo == 0){
                 $asis = new Asistencia();
                 $asis->id_user = Auth::user()->id;
@@ -89,10 +92,10 @@ class AsistenciaController extends Controller
                 return redirect()->back();
             }
 
-        /*}catch (\Exception $e){
+        }catch (\Exception $e){
             toastr()->error('¡Ocurrió un problema!');
             return redirect()->back();
-        }*/
+        }
     }
 
     /**
